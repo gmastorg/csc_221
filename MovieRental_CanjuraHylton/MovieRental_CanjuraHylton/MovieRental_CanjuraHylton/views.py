e5d4fdd58dea4ff4a0625dd9185193e553af3378
Gabriela <gmastorg@gmail.com>
2018-12-02 14:29:04 -0500

    updated version

--- MovieRental_CanjuraHylton/MovieRental_CanjuraHylton/MovieRental_CanjuraHylton/views.py
+++ MovieRental_CanjuraHylton/MovieRental_CanjuraHylton/MovieRental_CanjuraHylton/views.py
@@ -2,9 +2,19 @@
 Routes and views for the flask application.
 """
 
+from MovieRental_CanjuraHylton import listBuilder
+from MovieRental_CanjuraHylton import movies
+from MovieRental_CanjuraHylton import rentals
+from MovieRental_CanjuraHylton import store
 from datetime import datetime
 from flask import render_template
 from MovieRental_CanjuraHylton import app
+
+comments = []
+allMovies = []
+s = store.Store()
+
+app.config["DEBUG"] = True
 
 @app.route('/')
 @app.route('/home')
@@ -23,18 +33,30 @@ def contact():
         'contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
-    )
+        message=s.message,
+        store = s.name,
+        address = s.address,
+        city = s.city,
+        state = s.state,
+        zip = s.zipcode,
+        hours = s.hours,
+        phone = s.phone
+          )
 
-@app.route('/about')
+@app.route('/about', methods = ["GET", "POST"])
 def about():
     """Renders the about page."""
     return render_template(
         'about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
-    )
+        message='We are a video rental store. We have several movies for you to enjoy.',
+        comments = comments
+        )
+     
+    comments.append(request.form["contents"])
+    return redirect(url_for('about'))
+    
 
 @app.route('/login')
 def login():
@@ -43,27 +65,32 @@ def login():
         'login.html',
         title='Login',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Login to rent or return a movie.'
     )
+    redirect(url_for('login'))
 
-@app.route('/movies')
+@app.route('/movies', methods = ['GET', 'POST'])
 def movies():
-    """Renders the about page."""
+    """Renders the movies page."""
     return render_template(
         'movies.html',
         title='Movies',
         year=datetime.now().year,
-        message='Your application description page.'
-    )
+        message='Select from one of our many movies below:',
+        allMovies = listBuilder.getAllMovieTitles,
+    )     
+    redirect(url_for('movies'))
+
+
 
 @app.route('/rent')
 def rent():
     """Renders the about page."""
     return render_template(
-        'movies.html',
+        'rent.html',
         title='Rent',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Select a movie, the format and add it to your cart.'
     )
 
 @app.route('/returns')
@@ -73,5 +100,5 @@ def returns():
         'returns.html',
         title='Return',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Return outstanding movies.'
     )
